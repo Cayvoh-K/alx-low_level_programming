080949b8ef37796486cb617992e06dc870259fc6
Cayvoh-K <muhiakevin12@gmail.com>
2022-06-12 06:48:45 -0700

    printing all combination of ssingle digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/* more headers go there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+		if (number != 57)
+		{
+			putchar(' ');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
