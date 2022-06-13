822648e006fda491fe3562abaa02defc782613c1
Cayvoh-K <muhiakevin12@gmail.com>
2022-06-13 15:21:24 -0700

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * _puts -> this is a function that puts
- * @str: a param to _puts function
+ * _puts - prints a string
+ * @str: A pointer to an int that will be changed
+ * Return: void which means our answer is correct
  */
+
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-	_putchar('\n');
+char *c;
+int m;
+c = str;
+for (m = 0; c[m]; m++)
+{
+_putchar(c[m]);
+}
+_putchar('\n');
 }
