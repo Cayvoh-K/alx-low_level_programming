2f01cf722b9b8bba19511f8cb381f6626c09f0c9
Cayvoh-K <muhiakevin12@gmail.com>
2022-06-13 15:09:04 -0700

    returns length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen -> function to get the length of a string
- * @s: string pointer to passed to this function
- * Return: returns length of the string
+ * _strlen -length of a string
+ * @s: A pointer to an int that will be changed/updated
+ * Return: void that means our answer is correct
  */
 
 int _strlen(char *s)
 {
 int u;
 u = 0;
-while (s[u] != '\0)
+while (s[u] != '\0')
 {
 u++;
 }
