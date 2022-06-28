161fa806cdc953a0735bc0e4f9f7315f555cedc9
Cayvoh-K <muhiakevin12@gmail.com>
2022-06-28 13:43:42 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -69,7 +69,7 @@ for (w = 0; w < words; w++)
 while (str[index] == ' ')
 index++;
 
-letters = words_len(str + index);
+letters = word_len(str + index);
 
 strings[w] = malloc(sizeof(char) * (letters + 1));
 
