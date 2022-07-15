3685e5ff782cf6f9d8581cdae7a4c6cb9211d10b
Cayvoh-K <muhiakevin12@gmail.com>
2022-07-14 17:22:29 -0700

    hotfix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,13 @@
 #ifndef LISTS_H
+
 #define LISTS_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
 * @len: length of the string
 * @next: points to the next node
-*
-* Description: singly linked list node structure
+** Description: singly linked list node structure
 * for Alx project
 */
 
