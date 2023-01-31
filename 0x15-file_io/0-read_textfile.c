c44052984fb0197c3e34b892ead9aec19b7180ca
Cayvoh-K <muhiakevin12@gmail.com>
2023-01-31 06:34:37 +0000

    tread lightly she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX
- * @filename: filename
- * @letters: letters
- * Return: size_t
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t frd, fwr;
-	char *buff;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename)
-		fd = open(filename, O_RDONLY);
-	else
+	if (!filename)
 		return (0);
+
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * (letters));
-
-	if (buff)
-	{
-		frd = read(fd, buff, letters);
-		fwr = write(STDOUT_FILENO, buff, frd);
-	}
-	else
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
-	free(buff);
-	return (fwr);
+	free(buf);
+
+	return (nwr);
 }
